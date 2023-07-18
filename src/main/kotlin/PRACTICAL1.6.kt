ac7c756de8b47c6f118a8e7d8b90d0890496d50c
student <student@gnu.ac.in>
2023-07-18 10:09:51 +0530

    final

--- src/main/kotlin/PRACTICAL1.6.kt
+++ src/main/kotlin/PRACTICAL1.6.kt
@@ -1,4 +1,32 @@
-fun main()
-{
+fun addNums(a:Int , b:Int , c:Int):Int{
+    val sum:Int=a+b+c
+    println("sum of $a , $b , $c is :$sum")
+    return sum
+}
+fun subNums(a:Int , b:Int , c:Int):Int{
+    val sub:Int=a-b-c
+    println("Subtraction of $a , $b , $c is :$sub")
+    return sub
+}
+fun multNums(a:Int , b:Int , c:Int):Int{
+    val mult:Int=a*b*c
+    println("Multiplication of $a , $b , $c is :$mult")
+    return mult
+}
+fun divNums(a:Int , b:Int ):Float{
+    val div=a/b.toFloat()
+    println("Division of $a , $b is :$div")
+    return div
+}
 
+fun main(){
+    println("Enter 3 Numbers: ")
+    val a= readLine()?.toInt()!!
+    val b= readLine()?.toInt()!!
+    val c= readLine()?.toInt()!!
+
+    addNums(a,b,c)
+    subNums(a,b,c)
+    multNums(a,b,c)
+    divNums(a,b)
 }
