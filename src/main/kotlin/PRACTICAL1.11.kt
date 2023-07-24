63855fe0fd36ffb0dc76af2b4a9698457905747a
umang <umangraval0365@gmail.com>
2023-07-24 23:07:54 +0530

    24 JULY

--- src/main/kotlin/PRACTICAL1.11.kt
+++ src/main/kotlin/PRACTICAL1.11.kt
@@ -1,90 +1,96 @@
-class Matrix(private val data: Array<Array<Int>>) {
-    // Overload the + operator for matrix addition
-    operator fun plus(other: Matrix): Matrix {
-        require(data.size == other.data.size && data[0].size == other.data[0].size) {
-            "Matrices must have the same dimensions for addition."
+fun displayProduct(product: Array <IntArray>) {
+    println("Product of two matrices is: ")
+    for (row in product) {
+        for (column in row) {
+            print("$column    ")
         }
-
-        val result = Array(data.size) { i ->
-            Array(data[0].size) { j ->
-                data[i][j] + other.data[i][j]
-            }
-        }
-
-        return Matrix(result)
-    }
-
-    // Overload the - operator for matrix subtraction
-    operator fun minus(other: Matrix): Matrix {
-        require(data.size == other.data.size && data[0].size == other.data[0].size) {
-            "Matrices must have the same dimensions for subtraction."
-        }
-
-        val result = Array(data.size) { i ->
-            Array(data[0].size) { j ->
-                data[i][j] - other.data[i][j]
-            }
-        }
-
-        return Matrix(result)
-    }
-
-    // Overload the * operator for matrix multiplication
-    operator fun times(other: Matrix): Matrix {
-        require(data[0].size == other.data.size) {
-            "Number of columns in the first matrix must be equal to the number of rows in the second matrix for multiplication."
-        }
-
-        val result = Array(data.size) { i ->
-            Array(other.data[0].size) { j ->
-                var sum = 0
-                for (k in data[0].indices) {
-                    sum += data[i][k] * other.data[k][j]
-                }
-                sum
-            }
-        }
-
-        return Matrix(result)
-    }
-
-    override fun toString(): String {
-        val builder = StringBuilder()
-        for (row in data) {
-            builder.append(row.joinToString(" "))
-            builder.append("\n")
-        }
-        return builder.toString()
+        println()
     }
 }
 
-fun main() {
-    val matrix1 = Matrix(
-        arrayOf(
-            arrayOf(1, 2),
-            arrayOf(3, 4)
-        )
-    )
+fun multiplyMatrices(firstMatrix: Array <IntArray>,
+                     secondMatrix: Array <IntArray>,
+                     r1: Int,
+                     c1: Int,
+                     c2: Int): Array <IntArray> {
+    val product = Array(r1) { IntArray(c2) }
+    for (i in 0..r1 - 1) {
+        for (j in 0..c2 - 1) {
+            for (k in 0..c1 - 1) {
+                product[i][j] += firstMatrix[i][k] * secondMatrix[k][j]
+            }
+        }
+    }
 
-    val matrix2 = Matrix(
-        arrayOf(
-            arrayOf(5, 6),
-            arrayOf(7, 8)
-        )
-    )
+    return product
+}
 
-    val matrixSum = matrix1 + matrix2
-    val matrixDiff = matrix1 - matrix2
-    val matrixProduct = matrix1 * matrix2
+fun main(){
+    val rows = 3
+    val columns = 2
+    val firstMatrix = arrayOf(intArrayOf(6, 3), intArrayOf(9,0), intArrayOf(5, 4))
+    val secondMatrix = arrayOf(intArrayOf(2, 3), intArrayOf(-9, 0), intArrayOf(0, 4))
 
-    println("Matrix 1:")
-    println(matrix1)
-    println("Matrix 2:")
-    println(matrix2)
-    println("Matrix Sum:")
-    println(matrixSum)
-    println("Matrix Difference:")
-    println(matrixDiff)
-    println("Matrix Product:")
-    println(matrixProduct)
+    println("Matrix1: (3 * 2 Matrix): ")
+    for (row in firstMatrix) {
+        for (column in row) {
+            print("$column   ")
+        }
+        println()
+    }
+    println("Matrix2: (3 * 2 Matrix): ")
+    for (row in secondMatrix) {
+        for (column in row) {
+            print("$column   ")
+        }
+        println()
+    }
+
+    val sum = Array(rows) { IntArray(columns) }
+    for (i in 0..rows - 1) {
+        for (j in 0..columns - 1) {
+            sum[i][j] = firstMatrix[i][j] + secondMatrix[i][j]
+        }
+    }
+
+
+    println("Addition: (3 * 2 Matrix): ")
+    for (row in sum) {
+        for (column in row) {
+            print("$column    ")
+        }
+        println()
+    }
+
+    val sab = Array(rows) { IntArray(columns) }
+    for (i in 0..rows - 1) {
+        for (j in 0..columns - 1) {
+            sab[i][j] = firstMatrix[i][j] - secondMatrix[i][j]
+        }
+    }
+
+    println("Subtraction: (3 * 2 Matrix): ")
+    for (row in sab) {
+        for (column in row) {
+            print("$column    ")
+        }
+        println()
+    }
+
+    val r1 = 2
+    val c1 = 3
+    val r2 = 3
+    val c2 = 2
+    val firstMatrixx = arrayOf(intArrayOf(3, -2, 5), intArrayOf(3, 0, 4))
+    val secondMatrixx = arrayOf(intArrayOf(2, 3), intArrayOf(-9, 0), intArrayOf(0, 4))
+
+    val product = multiplyMatrices(firstMatrixx,
+        secondMatrixx,
+        r1,
+        c1,
+        c2)
+
+
+    displayProduct(product)
+
 }
