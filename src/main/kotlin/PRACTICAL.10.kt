e66ffbc70250ac42d4eb45cdf371891e1c4639b6
umang <umangraval0365@gmail.com>
2023-07-30 22:16:02 +0530

    24 JULY

--- src/main/kotlin/PRACTICAL.10.kt
+++ src/main/kotlin/PRACTICAL.10.kt
@@ -1,35 +1,53 @@
-class Car(var type: String, var model: String, var price: Double, var owner: String, var milesDrive: Double) {
-
-    fun getCarPrice(): Double {
-        return price
+class car(var carobjn:Int,var type:String,var model:String,var originalprice:Long,var currentprice:Long,var owner:String,var milesdrive:Long){
+    init {
+        println("Creating car class object car$carobjn in next line")
+        println("Object of class is created and Init is Called.")
     }
-
-    fun getOriginalCarPrice(): Double {
-
-        return price
+    fun getcarprice():Long{
+        return originalprice
     }
-
-    fun getCurrentCarPrice(): Double {
-
-        return price * 0.8
+    fun getcarinformation():Array<String>
+    {
+        var a1= arrayOf<String>(type,model,owner)
+        return a1
     }
-
-    fun getCarInformation(): String {
-        return "Type: $type\nModel: $model\nPrice: $price\nOwner: $owner\nMiles Driven: $milesDrive"
+    fun getoriginalprice():Long
+    {
+        return originalprice
     }
-
-    fun displayCarInfo() {
-        println(getCarInformation())
+    fun getcurrentprice():Long
+    {
+        return currentprice
+    }
+    fun displaycarinfo(){
+        println("----------")
+        var a=getcarinformation()
+        println("Car Information:${a[0]},${a[1]}")
+        println("Car Owner:${a[2]}")
+        println("Miles Drive:$milesdrive")
+        println("Original Car Price:$originalprice")
+        println("Current Car Price:$currentprice")
+        println("----------")
     }
 }
 
-fun main() {
+fun main()
+{
+    var no:Int=0;
+    no=no+1
+    var obj1=car(no,"BMW","2018",100000,98950,"Umang",105)
+    obj1.displaycarinfo()
+    no=no+1
+    var obj2=car(no,"BMW","2019",400000,350000,"Raja",20)
+    obj2.displaycarinfo()
 
-    val myCar = Car("Sedan", "Toyota Camry", 25000.0, "Umang Raval", 10000.0)
+    println("************ ArrayList of Car *****************")
+    no=no+1
+    var obj3=car(no,"Toyota","2017",1080000,1079000,"Yash",100)
+    no=no+1
+    var obj4=car(no,"Maruti","2020",4000000,3998000,"Nisarg",200)
 
-    println("Car Price: ${myCar.getCarPrice()}")
-    println("Original Car Price: ${myCar.getOriginalCarPrice()}")
-    println("Current Car Price: ${myCar.getCurrentCarPrice()}")
-
-    myCar.displayCarInfo()
-}
+    var person=arrayOf<car>(obj3,obj4)
+    for(i in person)
+    {
+        i.displaycarinfo()}}
--- src/main/kotlin/PRACTICAL1.11.kt
+++ src/main/kotlin/PRACTICAL1.11.kt
@@ -1,96 +1,85 @@
-fun displayProduct(product: Array <IntArray>) {
-    println("Product of two matrices is: ")
-    for (row in product) {
-        for (column in row) {
-            print("$column    ")
-        }
-        println()
-    }
-}
+import kotlin.math.min
 
-fun multiplyMatrices(firstMatrix: Array <IntArray>,
-                     secondMatrix: Array <IntArray>,
-                     r1: Int,
-                     c1: Int,
-                     c2: Int): Array <IntArray> {
-    val product = Array(r1) { IntArray(c2) }
-    for (i in 0..r1 - 1) {
-        for (j in 0..c2 - 1) {
-            for (k in 0..c1 - 1) {
-                product[i][j] += firstMatrix[i][k] * secondMatrix[k][j]
+class Matrix(var data:Array<IntArray>, var rows:Int, var cols:Int)
+{
+    operator fun plus(other: Matrix): Array<IntArray> {
+
+        val resultData = Array(rows) {IntArray(cols) }
+        for (i in 0 until rows) {
+            for (j in 0 until cols) {
+                resultData[i][j] = this.data[i][j] + other.data[i][j]
             }
         }
+        return resultData
     }
 
-    return product
+    operator fun minus(other: Matrix): Array<IntArray> {
+
+        val resultData = Array(rows) {IntArray(cols) }
+        for (i in 0 until rows) {
+            for (j in 0 until cols) {
+                resultData[i][j] = this.data[i][j] - other.data[i][j]
+            }
+        }
+        return resultData
+    }
+
+    operator fun times(other: Matrix): Array<IntArray> {
+
+        val resultData = Array(rows) {IntArray(cols) }
+        for (i in 0 until rows) {
+            for (j in 0 until other.cols) {
+                for (k in 0 until cols) {
+                    resultData[i][j] += this.data[i][k] * other.data[k][j]
+                }
+            }
+        }
+        return resultData
+    }
 }
-
-fun main(){
-    val rows = 3
-    val columns = 2
-    val firstMatrix = arrayOf(intArrayOf(6, 3), intArrayOf(9,0), intArrayOf(5, 4))
-    val secondMatrix = arrayOf(intArrayOf(2, 3), intArrayOf(-9, 0), intArrayOf(0, 4))
-
-    println("Matrix1: (3 * 2 Matrix): ")
-    for (row in firstMatrix) {
-        for (column in row) {
-            print("$column   ")
+fun print1(array:Array<IntArray>,row: Int,col: Int) {
+    println(" ( $row x $col Matrix):")
+    for (i in 0 until row)
+    {
+        for(j in 0 until col)
+        {
+            print("${array[i][j]}\t")
         }
         println()
     }
-    println("Matrix2: (3 * 2 Matrix): ")
-    for (row in secondMatrix) {
-        for (column in row) {
-            print("$column   ")
-        }
-        println()
-    }
+}
+fun main()
+{
+    val a1=arrayOf(intArrayOf(3,-2,5), intArrayOf(3,0,4))
+    var a2=arrayOf(intArrayOf(2,3), intArrayOf(-9,0), intArrayOf(0,4))
+    var a3=arrayOf(intArrayOf(6,3), intArrayOf(9,0), intArrayOf(5,4))
+    var firstmatrix=Matrix(a1,2,3)
+    var secondmatrix=Matrix(a2,3,2)
+    var secondmatrix1=Matrix(a3,3,2)
+    println("******************Addition*****************")
+    print("Matirx:1")
+    print1(a2,3,2)
+    print("Matrix:2")
+    print1(a3,3,2)
+    print("Addition:")
+    var plusmatrix=secondmatrix+secondmatrix1
+    print1(plusmatrix,3,2)
 
-    val sum = Array(rows) { IntArray(columns) }
-    for (i in 0..rows - 1) {
-        for (j in 0..columns - 1) {
-            sum[i][j] = firstMatrix[i][j] + secondMatrix[i][j]
-        }
-    }
+    println("******************Subtraction*****************")
+    print("Matirx:1")
+    print1(a3,3,2)
+    print("Matrix:2")
+    print1(a2,3,2)
+    print("Subtraction:")
+    var minusmatrix=secondmatrix1-secondmatrix
+    print1(minusmatrix,3,2)
 
-
-    println("Addition: (3 * 2 Matrix): ")
-    for (row in sum) {
-        for (column in row) {
-            print("$column    ")
-        }
-        println()
-    }
-
-    val sab = Array(rows) { IntArray(columns) }
-    for (i in 0..rows - 1) {
-        for (j in 0..columns - 1) {
-            sab[i][j] = firstMatrix[i][j] - secondMatrix[i][j]
-        }
-    }
-
-    println("Subtraction: (3 * 2 Matrix): ")
-    for (row in sab) {
-        for (column in row) {
-            print("$column    ")
-        }
-        println()
-    }
-
-    val r1 = 2
-    val c1 = 3
-    val r2 = 3
-    val c2 = 2
-    val firstMatrixx = arrayOf(intArrayOf(3, -2, 5), intArrayOf(3, 0, 4))
-    val secondMatrixx = arrayOf(intArrayOf(2, 3), intArrayOf(-9, 0), intArrayOf(0, 4))
-
-    val product = multiplyMatrices(firstMatrixx,
-        secondMatrixx,
-        r1,
-        c1,
-        c2)
-
-
-    displayProduct(product)
-
+    println("******************Subtraction*****************")
+    print("Matirx:1")
+    print1(a1,2,3)
+    print("Matrix:2")
+    print1(a2,3,2)
+    print("Multiplication:")
+    var multimatrix=firstmatrix*secondmatrix
+    print1(multimatrix,2,2)
 }
